b0e5a70cf84e270e848c1a3ec88b99389487cb7b
jphomick <jphomick@gmail.com>
2019-07-18 16:34:04 -0400

    Search function working

--- src/main/java/com/joseph/customer/CustomerRepository.java
+++ src/main/java/com/joseph/customer/CustomerRepository.java
@@ -1,6 +1,10 @@
 package com.joseph.customer;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
+@Component
 public interface CustomerRepository extends CrudRepository<Customer, Long>{
 }
--- src/main/java/com/joseph/customer/HomeController.java
+++ src/main/java/com/joseph/customer/HomeController.java
@@ -71,12 +71,81 @@ public class HomeController {
         return "customerform";
     }
 
+    @GetMapping("/search")
+    public String searchForm(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "search";
+    }
+
+    @PostMapping("/results")
+    public String searchResults(Model model, Customer customer) {
+        ArrayList<Customer> found = new ArrayList<>();
+
+        for (Customer foundPerson : customerRepository.findAll()) {
+            boolean match = true;
+            if (!customer.getFirst_name().equals("") && !foundPerson.getFirst_name().contains(customer.getFirst_name())) {
+                match = false;
+            }
+            if (!customer.getLast_name().equals("") && !foundPerson.getLast_name().contains(customer.getLast_name())) {
+                match = false;
+            }
+            if (!customer.getTitle().equals("") && !foundPerson.getTitle().contains(customer.getTitle())) {
+                match = false;
+            }
+            if (!customer.getStreet_address().equals("") && !foundPerson.getStreet_address().contains(customer.getStreet_address())) {
+                match = false;
+            }
+            if (!customer.getZip_code().equals("") && !foundPerson.getZip_code().contains(customer.getZip_code())) {
+                match = false;
+            }
+            if (!customer.getEmail_address().equals("") && !foundPerson.getEmail_address().contains(customer.getEmail_address())) {
+                match = false;
+            }
+
+            if (match) {
+                found.add(foundPerson);
+            }
+        }
+
+        model.addAttribute("customers", found);
+
+        ArrayList<String> states = new ArrayList<>();
+        int curr = 1;
+        for (State state : stateRepository.findAll()) {
+            states.add(stateRepository.findById((long) curr).get().getLetter_code());
+            curr++;
+        }
+        model.addAttribute("states", states);
+        ArrayList<String> cities = new ArrayList<>();
+        curr = 1;
+        for (City city : cityRepository.findAll()) {
+            cities.add(cityRepository.findById((long) curr).get().getName());
+            curr++;
+        }
+        model.addAttribute("cities", cities);
+        ArrayList<String> positions = new ArrayList<>();
+        curr = 1;
+        for (Position position : positionRepository.findAll()) {
+            positions.add(positionRepository.findById((long) curr).get().getName());
+            curr++;
+        }
+        model.addAttribute("positions", positions);
+        ArrayList<String> companies = new ArrayList<>();
+        curr = 1;
+        for (Company company : companyRepository.findAll()) {
+            companies.add(companyRepository.findById((long) curr).get().getName());
+            curr++;
+        }
+        model.addAttribute("companies", companies);
+        return "list";
+    }
+
     @PostMapping("/process")
-    public String processForm(@Valid Customer course, BindingResult result) {
+    public String processForm(@Valid Customer customer, BindingResult result) {
         if (result.hasErrors()){
             return "customerform";
         }
-        customerRepository.save(course);
+        customerRepository.save(customer);
         return "redirect:/";
     }
 
